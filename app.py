6782610a9d3b6c9fd4683a8acb4bbc343f59e19c
Jaider Aprilla <jaiderreyes273@gmail.com>
2025-05-12 19:53:43 -0500

    se carga la variable del entorno en el en ael archivi app.py y tambien se crea la app el el archivo app.py

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ import os
 from musica_api import create_app
 from dotenv import load_dotenv
 
-# TODO: Cargar variables de entorno desde archivo .env si existe
+# Cargar variables de entorno desde archivo .env si existe
+load_dotenv()
 
-# TODO: crear la aplicación
+# Crear la aplicación
+app = create_app()
+
 
 if __name__ == "__main__":
     # TODO: Obtener puerto del ambiente o usar 5000 por defecto
