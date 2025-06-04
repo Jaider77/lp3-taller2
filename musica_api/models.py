c31c545070832212292cb71bc723950659b23304
Jaider Aprilla <jaiderreyes273@gmail.com>
2025-06-04 18:58:47 -0500

    se elimino el frontend, ya que estara en el taller #3

--- musica_api/models.py
+++ musica_api/models.py
@@ -11,17 +11,10 @@ class Usuario(db.Model):
     """
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100), nullable=False)
-    correo = db.Column(db.String(100), unique=True, nullable=False)
-    fecha_registro = db.Column(db.DateTime, default=datetime.utcnow)
-
-    # este campo es para definir al adnmin o cliente
-class Usuario(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(100), nullable=False)
     correo = db.Column(db.String(120), unique=True, nullable=False)
     rol = db.Column(db.String(20), default="cliente")  # "admin" o "cliente"
+    fecha_registro = db.Column(db.DateTime, default=datetime.utcnow)
 
-    
     
     # Relación con favoritos
     favoritos = db.relationship("Favorito", back_populates="usuario", cascade="all, delete-orphan")
